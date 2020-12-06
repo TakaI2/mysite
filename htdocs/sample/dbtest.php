692c2d9fd73c8691296c464d2d518d9fd75e9aad
TakaI2 <110tic2z@gmail.com>
2020-12-06 17:38:32 +0900

    フォームのテスト、PDO通信のテストを追加

---
+++ htdocs/sample/dbtest.php
@@ -0,0 +1,10 @@
+﻿<?php
+$pdo=new PDO('mysql:host=mysql5.star.ne.jp;dbname=ss742955_casavd;charset=utf8','ss742955_casavd','df853zf');
+
+print 'PDOクラスによる接続に成功しました。';
+
+//ここでデータベース関連の処理をする。
+
+$pdo = null;
+
+?>
---
+++ htdocs/view.php
@@ -0,0 +1,16 @@
+﻿<!DOCTYPE html>
+<html lang="ja">
+<head>
+<title>PHPのテスト</title>
+</head>
+<body>
+
+<?php
+print $_POST["onamae"]."さん、からのメッセージ";
+print"<br><br>";
+print"本文:<br>";
+print nl2br($_POST["honbun"],false);
+?>
+
+</body>
+</html>
